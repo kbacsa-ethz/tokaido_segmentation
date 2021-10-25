5fda94c14af5170ceb56dc39ace85ffee8759f69
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-10-25 14:16:36 +0200

    added dropout to segmentation head as well

--- segmentation_models_pytorch/base/heads.py
+++ segmentation_models_pytorch/base/heads.py
@@ -6,9 +6,10 @@ class SegmentationHead(nn.Sequential):
 
     def __init__(self, in_channels, out_channels, kernel_size=3, activation=None, upsampling=1):
         conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
+        dropout = nn.Dropout2d(0.2, inplace=True)
         upsampling = nn.UpsamplingBilinear2d(scale_factor=upsampling) if upsampling > 1 else nn.Identity()
         activation = Activation(activation)
-        super().__init__(conv2d, upsampling, activation)
+        super().__init__(conv2d, upsampling, dropout, activation)
 
 
 class ClassificationHead(nn.Sequential):
