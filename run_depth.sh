c377649c4dca81ad99e2cb6451300a49c20260c1
Kiran Bacsa <kiran.bacsa@sec.ethz.ch>
2021-11-02 13:57:12 +0100

    typo

--- run_depth.sh
+++ run_depth.sh
@@ -1,6 +1,6 @@
 # Change data path accordingly
 DATADIR="/app/data"
-python train.py --backbone resnet50 --root-path $PWD --data-path "$DATADIR" --device cuda --batch-size 8 --num-workers 8 --n-epochs 300 --lmdb
+python train_depth.py --backbone resnet50 --root-path $PWD --data-path "$DATADIR" --device cuda --batch-size 8 --num-workers 8 --n-epochs 300 --lmdb
 MODELDIR=$(ls -td $PWD/runs/* | head -1)
 MODELDIR="$MODELDIR/best_model.pth"
 echo "Running evaliation and test on $MODELDIR"
